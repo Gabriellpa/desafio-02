98e20d819cd772cfedf098e3ffff2b95a9abce00
Gabriellpa <gabriellpantunes@gmail.com>
2020-11-23 23:03:49 -0300

    Implementação das rotas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4 } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: v4(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(e => e.id === id);
+
+  if(repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+  repositories[repositoryIndex] = {
+    id,
+    title: title || repositories[repositoryIndex].title,
+    url: url || repositories[repositoryIndex].url,
+    techs: techs || repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  return response.send(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(e => e.id === id);
+
+  if(repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repositoryIndex,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(e => e.id === id);
+
+  if(repositoryIndex === -1) {
+    return response.status(400).send();
+  }
+
+  repositories[repositoryIndex].likes++;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
